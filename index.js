df74ce0efb6068ee15ea44e4fb4c84f788c9f321
narekhovhannisyan <narekgilmour@gmail.com>
2017-09-08 11:48:19 +0400

    cpu usage and memory usage functions are sync, added logResourcesUsage + average functions

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 const os = require('os')
 const Promise = require('bluebird')
 const execFile = require('child_process').execFile
+const execFileSync = require('child_process').execFileSync
 const platform = require('os').platform()
 const path = require('path')
 const wmic = platform === 'win32' ? path.join(process.env.SystemRoot, 'System32', 'wbem', 'wmic.exe') : null
 
-const caller = () => {
-  const percentages = []
-  const timerId = setInterval(() => {
-    if (percentages.length <= 20) {
-      getPercentage((err, res) => { if (err) { console.error(err) } percentages.push(res) })
-    } else {
-      const average = percentages.reduce((previous, current) => {
-        return current + previous
-      }, 0) / percentages.length
-      console.log(average)
-      clearInterval(timerId)
-    }
-  }, 1000)
-  return percentages
-}
-
-const getPercentage = (cb) => {
-  try {
-    execFile(wmic, ['cpu', 'get', 'loadpercentage'], (error, res, stderr) => {
-      if (error || stderr) {
-        throw error
-      } else {
-        const percentage = res.match(/\d+/g)[0]
-        return cb(null, percentage)
-      }
-    })
-  } catch (error) {
-    console.error(error)
-  }
+const getCpuPercentage = () => {
+  const cpuLoadPercentage = execFileSync(wmic, ['cpu', 'get', 'loadpercentage'])
+  return cpuLoadPercentage.toString().match(/\d+/g)[0]
 }
 
 const getMemoryUsage = () => {
-  console.log(((os.totalmem() - os.freemem()) / (1024 * 1024 * 1024)).toFixed(2))
-  return Promise.resolve(((os.totalmem() - os.freemem()) / (1024 * 1024 * 1024)).toFixed(2))
+  return ((os.totalmem() - os.freemem()) / (1024 * 1024 * 1024)).toFixed(2)
 }
 
-caller()
+const average = (arr) => {
+  return arr.reduce((sum, current) => {
+    return Number(sum) + Number(current)
+  })
+}
+
+const logResourcesUsage = (callCount, interval) => {
+  const cpuLoadPercentages = []
+  const memoryUsagePercentages = []
+  const timerId = setInterval(() => {
+    if (cpuLoadPercentages.length < callCount) {
+      const cpuPercentage = getCpuPercentage()
+      cpuLoadPercentages.push(cpuPercentage)
+      const memoryUsage = getMemoryUsage()
+      memoryUsagePercentages.push(memoryUsage)
+    } else {
+      clearInterval(timerId)
+      const cpuPercentage = average(cpuLoadPercentages)
+      const memoryUsagePercentage = average(memoryUsagePercentages)
+      console.log('CPU usage percentage: ' + (cpuPercentage / cpuLoadPercentages.length).toFixed(2))
+      console.log('Memory usage in GB: ' + (memoryUsagePercentage / memoryUsagePercentages.length).toFixed(2))
+    }
+  }, interval)
+}
+
+module.exports = {
+  logResourcesUsage
+}
