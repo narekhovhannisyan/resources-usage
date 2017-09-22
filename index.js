8a733407900cad9408ba5a109dc17f34845da855
narekhovhannisyan <narekgilmour@gmail.com>
2017-09-22 12:14:11 +0400

    added cpuAverage function for all kind of os

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const os = require('os')
-const execFileSync = require('child_process').execFileSync
-const platform = require('os').platform()
 const path = require('path')
+const platform = require('os').platform()
+const execFileSync = require('child_process').execFileSync
 const wmic = platform === 'win32' ? path.join(process.env.SystemRoot, 'System32', 'wbem', 'wmic.exe') : null
+
+const cpuAverage = () => {
+  let totalIdle = 0, totalTick = 0
+  const cpus = os.cpus()
+  for(let i = 0, len = cpus.length; i < len; i++) {
+    const cpu = cpus[i]
+    for(type in cpu.times) {
+      totalTick += cpu.times[type]
+    }     
+    totalIdle += cpu.times.idle
+  }
+  return {idle: totalIdle / cpus.length,  total: totalTick / cpus.length}
+}
 
 const getWindowsCpuPercentage = () => {
   const cpuLoadPercentage = execFileSync(wmic, ['cpu', 'get', 'loadpercentage'])
@@ -19,13 +32,27 @@ const average = (arr) => {
   })
 }
 
+/**
+ * gets object with two properties: cpuPercentage and memoryUsage
+ * cpuPercentages shows usage in percents
+ * memoryUsage shows RAM memory usage in gb's
+ * @param {*} callCount call count in interval
+ * @param {*} interval interval in milliseconds
+ * @param {*} cb call back function
+ */
 const resourcesUsage = (callCount, interval, cb) => {
   const cpuLoadPercentages = []
   const memoryUsagePercentages = []
   const timerId = setInterval(() => {
     if (cpuLoadPercentages.length < callCount) {
-      const cpuPercentage = getWindowsCpuPercentage()
-      cpuLoadPercentages.push(cpuPercentage)
+    var startMeasure = cpuAverage()
+    setTimeout(() => { 
+      const endMeasure = cpuAverage(); 
+      const idleDifference = endMeasure.idle - startMeasure.idle
+      const totalDifference = endMeasure.total - startMeasure.total
+      const percentageCPU = 100 - ~~(100 * idleDifference / totalDifference)
+      cpuLoadPercentages.push(percentageCPU)
+    }, 100)
       const memoryUsage = getMemoryUsage()
       memoryUsagePercentages.push(memoryUsage)
     } else {
