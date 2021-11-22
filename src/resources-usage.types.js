ec7a82bbd4c45fa5ba4a821e190d9f21d5c4b32a
narekhovhannisyan <narekgilmour@gmail.com>
2021-11-22 16:48:09 +0400

    src: init types.

---
+++ src/resources-usage.types.js
@@ -0,0 +1,23 @@
+/**
+ * @namespace ResourcesUsageTypes
+ */
+
+/**
+ * @typedef CPUAverage
+ * @property {Number} idle
+ * @property {Number} total
+ */
+
+/**
+ * @typedef CPUArrayAverage
+ * @property {Number} cpuLoadPercentage
+ * @property {Number} busyMemoryPercentage
+ */
+
+/**
+ * @typedef StatisticsData
+ * @property {Number} cpuLoad
+ * @property {Number} busyMemory
+ */
+
+module.exports = {}
